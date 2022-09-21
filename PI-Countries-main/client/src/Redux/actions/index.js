5eba0c0dfbd57b52a6c94b902ed0f6089fd1664e
unknown <fernandaformentod@gmail.com>
2022-09-21 00:20:04 -0300

    Reducer configurado

---
+++ PI-Countries-main/client/src/Redux/actions/index.js
@@ -0,0 +1,52 @@
+export const GET_ALL_COUNTRIES = "GET_ALL_COUNTRIES";
+export const GET_COUNTRY = "GET_COUNTRY";
+export const GET_COUNTRY_BY_NAME = "GET_COUNTRY_BY_NAME";
+export const CREATE_ACTIVITY = "CREATE_ACTIVITY";
+
+export const getAllCountries = () => (dispatch) => {
+  return fetch("http://localhost:3001/countries")
+    .then((data) => data.json())
+    .then((data) =>
+      dispatch({
+        type: GET_ALL_COUNTRIES,
+        payload: data,
+      })
+    )
+    .catch((err) => console.log(err.message));
+};
+
+export const getCountry = (id) => (dispatch) => {
+  return fetch(`http://localhost:3001/countries/${id}`)
+    .then((data) => data.json())
+    .then((data) =>
+      dispatch({
+        type: GET_COUNTRY,
+        payload: data,
+      })
+    )
+    .catch((err) => console.log(err.message));
+};
+
+export const getCountryByName = (name) => (dispatch) => {
+  return fetch(`http://localhost:3001/countries?name=${name}`)
+    .then((data) => data.json())
+    .then((data) =>
+      dispatch({
+        type: GET_COUNTRY_BY_NAME,
+        payload: data,
+      })
+    )
+    .catch((err) => console.log(err.message));
+};
+
+export const createActivity = () => (dispatch) => {
+  return fetch("http://localhost:3001/activities")
+    .then((data) => data.json())
+    .then((data) =>
+      dispatch({
+        type: CREATE_ACTIVITY,
+        payload: data,
+      })
+    )
+    .catch((err) => console.log(err.message));
+};
---
+++ PI-Countries-main/client/src/Redux/reducer/index.js
@@ -0,0 +1,43 @@
+import {
+  GET_ALL_COUNTRIES,
+  GET_COUNTRY,
+  GET_COUNTRY_BY_NAME,
+  CREATE_ACTIVITY,
+} from "../actions";
+
+const initialState = {
+  country: {},
+  countries: [],
+  allCountries: [],
+  activities: [],
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ALL_COUNTRIES:
+      return {
+        ...state,
+        countries: action.payload,
+        allCountries: action.payload,
+      };
+    case GET_COUNTRY:
+      return {
+        ...state,
+        country: action.payload,
+      };
+    case GET_COUNTRY_BY_NAME:
+      return {
+        ...state,
+        country: action.payload,
+      };
+    case CREATE_ACTIVITY:
+      return {
+        ...state,
+        activities: [...state.activities, action.payload],
+      };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
