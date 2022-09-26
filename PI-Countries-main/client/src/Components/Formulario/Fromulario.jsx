53e3362c75d5887fead718868cdd6c7765eb6d73
unknown <fernandaformentod@gmail.com>
2022-09-26 17:58:43 -0300

    Formulario casi listo!

--- PI-Countries-main/client/src/Components/Formulario/Fromulario.jsx
+++ PI-Countries-main/client/src/Components/Formulario/Fromulario.jsx
@@ -6,17 +6,108 @@ import s from "../Formulario/Formulario.module.css";
 
 export const CreateActivity = () => {
   const countries = useSelector((state) => state.countries);
+  const [name, setName] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [duration, setDuration] = useState("");
+  const [season, setSeason] = useState("");
+  const [selectCountries, setSelectCountries] = useState([]);
+  const [errorName, setErrorName] = useState("");
+  const [errorDifficulty, setErrorDifficulty] = useState("");
+  const [errorDuration, setErrorDuration] = useState("");
+  const [errorSeason, setErrorSeason] = useState("");
+  const [errorSelectCountries, setErrorSelectCountries] = useState("");
+
+  const handleName = (name) => {
+    setName(name);
+  };
+
+  const handleDifficulty = (difficulty) => {
+    setDifficulty(difficulty);
+  };
+
+  const handleDuration = (duration) => {
+    setDuration(duration);
+  };
+
+  const handleSeason = (season) => {
+    setSeason(season);
+  };
+
+  const handleSelectCountries = (countries) => {
+    if (selectCountries.length === 0) {
+      setSelectCountries([...selectCountries, countries]);
+    } else {
+      let countriesFiltrados = selectCountries.filter((e) => e === countries);
+      if (countriesFiltrados.length === 0) {
+        setSelectCountries([...selectCountries, countries]);
+      }
+    }
+  };
+
+  const dispatch = useDispatch();
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (
+      errorName === "" &&
+      errorDifficulty === "" &&
+      errorDuration === "" &&
+      errorSeason === "" &&
+      errorSelectCountries === "" &&
+      name !== "" &&
+      difficulty !== "" &&
+      duration !== "" &&
+      season !== "" &&
+      selectCountries !== ""
+    ) {
+      let inputsOrdenado = {
+        name: name,
+        difficulty: difficulty,
+        duration: duration,
+        season: season,
+        countries: selectCountries.map((el) => el.id),
+      };
+      dispatch(createActivity(inputsOrdenado));
+
+      alert("Congratulations! Your activity has been successfully created.");
+    }
+  }
 
   return (
     <div className={s.divContainer}>
-      <form className={s.form}>
+      <form className={s.form} onSubmit={(e) => handleSubmit(e)}>
         <div>
           <label className={s.divLabel}>Name:</label>
-          <input className={s.inputs} />
+          <input
+            className={s.inputs}
+            value={name}
+            onChange={(e) => {
+              handleName(e.target.value);
+              if (
+                /^[A-Za-z\s]+$/g.test(e.target.value.trim()) ||
+                e.target.value === ""
+              ) {
+                setErrorName("");
+              } else {
+                setErrorName("Only letters.");
+              }
+            }}
+          />
         </div>
+        <span>{errorName}</span>
         <div>
           <label className={s.divLabel}>Difficulty:</label>
-          <select>
+          <select
+            value={difficulty}
+            onChange={(e) => {
+              if (e.target.value === "-") {
+                setErrorDifficulty("You must select a number between 1 and 5");
+              } else {
+                setErrorDifficulty("");
+                handleDifficulty(e.target.value);
+              }
+            }}
+          >
+            <option>-</option>
             <option>1</option>
             <option>2</option>
             <option>3</option>
@@ -24,25 +115,68 @@ export const CreateActivity = () => {
             <option>5</option>
           </select>
         </div>
+        <span>{errorDifficulty}</span>
         <div>
           <label className={s.divLabel}>Duration:</label>
-          <input className={s.inputs} />
+          <input
+            className={s.inputs}
+            value={duration}
+            onChange={(e) => {
+              handleDuration(e.target.value);
+              if (
+                (/^\d+$/.test(e.target.value) &&
+                  e.target.value > 0 &&
+                  e.target.value < 73) ||
+                e.target.value === ""
+              ) {
+                setErrorDuration("");
+              } else {
+                setErrorDuration(
+                  "Only positive numbers and between 0 and 72 hours"
+                );
+              }
+            }}
+          />
+          <span>HS.</span>
         </div>
+        <span>{errorDuration}</span>
         <div>
           <label className={s.divLabel}>Season:</label>
-          <select>
+          <select
+            value={season}
+            onChange={(e) => {
+              if (e.target.value === "-") {
+                setErrorSeason("You must select a season.");
+              } else {
+                setErrorSeason("");
+                handleSeason(e.target.value);
+              }
+            }}
+          >
+            <option>-</option>
             <option>Summer</option>
             <option>Spring</option>
             <option>Winter</option>
             <option>Autumn</option>
           </select>
         </div>
+        <span>{errorSeason}</span>
         <div>
           <label className={s.divLabel}>Countries:</label>
-          <select>
+          <select
+            onChange={(e) => {
+              if (e.target.value === "-") {
+                setErrorSelectCountries("You must select a country.");
+              } else {
+                setErrorSelectCountries("");
+                handleSelectCountries(e.target.value);
+              }
+            }}
+          >
+            <option>-</option>
             {countries.length > 0 ? (
               countries.map((el) => (
-                <option id={el.id} key={el.id}>
+                <option id={el.id} key={el.id} value={el.id}>
                   {el.name}
                 </option>
               ))
@@ -51,6 +185,7 @@ export const CreateActivity = () => {
             )}
           </select>
         </div>
+        <span>{errorSelectCountries}</span>
         <div>
           <input type="submit"></input>
         </div>
